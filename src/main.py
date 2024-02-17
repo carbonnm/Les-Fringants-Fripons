2e28f63e29816c99adc12e6b69c1769f6f9819a7
Djingoo <56677342+Djingoo@users.noreply.github.com>
2024-02-17 15:37:26 +0100

    Deletion of PyQt

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from guis.login_window import LoginFrame
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    login_frame = LoginFrame()
-    login_frame.showMaximized()
-    sys.exit(app.exec())
