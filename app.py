1b9fc69ecced666e8e677f8168c04f042e6a5743
ZeTioZ <donato_gentile@hotmail.it>
2024-02-17 17:53:24 +0100

    New header

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ from __init__ import app
 
 if __name__ == '__main__':
     app.route('/')(routes.index)
+    app.route('/login', methods=["GET", "POST"])(routes.login)
+    app.route('/logout')(routes.logout)
+    app.errorhandler(404)(routes.page_not_found)
+    app.errorhandler(500)(routes.server_error)
     app.run()
